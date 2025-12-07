e89acdccddadea1db347333799e46a0bbebc02e8
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-07 23:42:31 +0300

    worker/index.ts: fixes the issue with failing to add name of the index file on directory visits

--- worker/index.ts
+++ worker/index.ts
@@ -2,20 +2,40 @@ export interface Env {
   ASSETS: Fetcher;
 }
 
+function normalizeDirectories(request: Request): Request {
+  const url = new URL(request.url);
+  if (url.pathname.endsWith("/")) {
+    url.pathname += "index.html";
+    return new Request(url.toString(), request);
+  }
+  return request;
+}
+
+function isHomepage(request: Request): boolean {
+  const url = new URL(request.url);
+  return url.pathname === "/index.html";
+}
+
+async function handleNotFound(request: Request, env: Env): Promise<Response> {
+  if (isHomepage(request)) {
+    // already tried the homepage
+    throw Error("homepage not found");
+  }
+  return Response.redirect(new URL(request.url).origin, 302);
+}
+
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
-    const url = new URL(request.url);
-    if (url.pathname.endsWith("/")) url.pathname += "index.html";
-
-    const asset = await env.ASSETS.fetch(request);
-    if (asset.status === 404) {
-      if (url.pathname !== "/index.html") {
-        const home = await env.ASSETS.fetch(new Request(new URL("/index.html", url.origin).toString(), request));
-        if (home.status === 200) return home;
+    try {
+      request = normalizeDirectories(request);
+      const response = await env.ASSETS.fetch(request);
+      if (response.status === 404) {
+        return await handleNotFound(request, env);
       }
+      return response;
+    } catch (e: any) {
+      console.log({ "unit": "router", "statement": "main-try-catch", "e": e });
       return new Response("Internal Server Error", { status: 500 });
     }
-
-    return asset;
   },
 };
