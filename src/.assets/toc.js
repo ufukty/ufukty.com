b68a7d1dcce4ed126a38d3b7f0bf05ee0d47671b
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-04-06 19:48:19 +0300

    src: fixes the JS script toggle ToC.

--- src/.assets/toc.js
+++ src/.assets/toc.js
@@ -3,7 +3,7 @@
 const toc = document.getElementById("toc");
 
 function toggle() {
-  toc.style.display = document.documentElement.scrollHeight >= 2 * window.innerHeight ? "visible" : "hidden";
+  toc.style.display = document.documentElement.scrollHeight >= 2 * window.innerHeight ? "block" : "none";
 }
 
 window.addEventListener("resize", toggle);
