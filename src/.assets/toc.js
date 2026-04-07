71a1f3db35fab51ecd6f52b195d45505f8ad282d
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-04-07 20:23:38 +0300

    src/as/toc.js: formats.

--- src/.assets/toc.js
+++ src/.assets/toc.js
@@ -2,13 +2,13 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const toc = document.getElementById("toc");
-  
+
   function toggle() {
     const isWide = window.innerWidth > 1000;
     const isTall = document.documentElement.scrollHeight >= 2 * window.innerHeight;
     toc.style.display = isWide && isTall ? "block" : "none";
   }
-  
+
   window.addEventListener("resize", toggle);
   toggle();
 });
